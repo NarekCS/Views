c758c649416e887f8b53139f0f53ab0d4325491b
narek.avanesyan <narek.avanesyan@FC-PROG-16>
2018-09-07 16:22:15 +0400

    Razor

---
+++ Views/Infrastructure/RazorCreatedClassIndex.cs
@@ -0,0 +1,33 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Razor;
+using Microsoft.AspNetCore.Mvc.Razor.Internal;
+using Microsoft.AspNetCore.Mvc.Rendering;
+namespace Asp
+{
+    public class ASPV_Views_Home_Index_cshtml : RazorPage<string[]>
+    {
+        public IUrlHelper Url { get; private set; }
+        public IViewComponentHelper Component { get; private set; }
+        public IJsonHelper Json { get; private set; }
+        public IHtmlHelper<string[]> Html { get; private set; }
+        public override async Task ExecuteAsync()
+        {
+            Layout = null;
+            WriteLiteral(@"<!DOCTYPE html><html><head> 
+                    <meta name=""viewport"" content=""width=device-width"" />  
+                    <title>Razor</title> 
+                    <link asp-href-include=""lib/bootstrap/dist/css/*.min.css""
+                    rel=""stylesheet"" />     
+                    </head><body class=""m-1 p-1"">This is a list of fruit names:");
+            foreach (string name in Model)
+            {
+                WriteLiteral("<span><b>");
+                Write(name);
+                WriteLiteral("</b></span>");
+            }
+            WriteLiteral("</body></html>");
+        }
+    }
+}
+
--- Views/Startup.cs
+++ Views/Startup.cs
@@ -5,7 +5,10 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Razor;
 using Microsoft.Extensions.DependencyInjection;
+using Views.Infrastructure;
 
 namespace Views
 {
@@ -16,6 +19,14 @@ namespace Views
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+            //services.Configure<MvcViewOptions>(options => {  
+            //    options.ViewEngines.Clear(); 
+            //    options.ViewEngines.Insert(0, new DebugDataViewEngine());  
+            //}); 
+            services.Configure<RazorViewEngineOptions>(options => { 
+                     options.ViewLocationExpanders.Add(new SimpleExpander());
+                     options.ViewLocationExpanders.Add(new ColorExpander());
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
